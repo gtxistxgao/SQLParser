6b4fc4936df9a5f4061ea5b82a19c1174d5edacb
FrankisFine <zjcxgaoxing@gmail.com>
2016-04-05 11:27:12 -0500

    Add Comment
    Finished Select From Where

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,10 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 /**
- * @author Tianxiang Gao
+ *
+ * 2016 Apr 5, 2016 Main.java
+ * 
+ * @author: TianxiangGao
  *
  */
 
@@ -14,26 +17,43 @@ public class Main {
 	private static BufferedWriter bufferedWriter;
 	private static BufferedReader bufferedReader;
 
+	/**
+	 * @param args
+	 * @throws Exception
+	 * @description: this method will read the query command from input.txt and
+	 *               parse it then output the resut to AST.xml file
+	 *
+	 */
 	public static void main(String args[]) throws Exception {
-		String inputPath = "/Users/frankgao/Documents/COMS661/input.txt";
-		String outputPath = "/Users/frankgao/Documents/COMS661/AST.xml";
-		System.out.println("read Query from \"" + inputPath + "\"");
+		String inputPath = "input.txt";
+		String outputPath = "AST.xml";
+		System.out.println("read Query from \n\"" + inputPath + "\"");
 		String input = readCommand(inputPath);
 		System.out.println("input Query: ");
 		System.out.println("---------------------------------------");
 		System.out.println(input);
+		// CALL the parse method and parse the input query then assign the AST
+		// to rst String.
 		String rst = parse(input);
 		System.out.println("\n---------------------------------------");
 		System.out.println("AST.xml are output as following : ");
 		System.out.println("---------------------------------------\n");
+		// print out rst String(AST)
 		System.out.println(rst);
 		System.out.println("\n---------------------------------------");
-		System.out.println("Start to write into file :\n" + outputPath);
+		System.out.println("Start to write into file :\n\"" + outputPath + "\"");
 		System.out.println("---------------------------------------");
+		// output the rst into AST.xml file
 		outputAST(rst, outputPath);
 		System.out.println("---------------------------------------");
 	}
 
+	/**
+	 * @param input
+	 * @return AST
+	 * @description: this method parse the input Query String
+	 *
+	 */
 	private static String parse(String input) {
 		String rst = "";
 		try {
@@ -45,6 +65,13 @@ public class Main {
 		return rst;
 	}
 
+	/**
+	 * @param inputPath
+	 * @return Query String
+	 * @description: this method will read the query from the file of
+	 *               "inputPath" and return the Query String
+	 *
+	 */
 	private static String readCommand(String inputPath) {
 		String input = "";
 		try {
@@ -65,6 +92,13 @@ public class Main {
 		return input;
 	}
 
+	/**
+	 * @param rst
+	 * @param outputPath
+	 * @description: This method will output the AST into the file of
+	 *               "outputPath"
+	 *
+	 */
 	private static void outputAST(String rst, String outputPath) {
 		FileWriter writer;
 		try {
